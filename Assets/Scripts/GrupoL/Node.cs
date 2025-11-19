b85c5a767f5e84d4201fdf41e85fa518aa718bb0
Sara <samepae01@gmail.com>
2025-11-19 18:03:01 +0100

    Cambios monis

--- Assets/Scripts/GrupoL/Node.cs
+++ Assets/Scripts/GrupoL/Node.cs
@@ -1,31 +1,26 @@
+using System.Collections.Generic;
+using Navigation.Interfaces;
+using Navigation.World;
 using UnityEngine;
-using System.Collections;
 
-public class Node
+namespace GrupoL
 {
-    public bool walkable;
-    public Vector3 worldPos;//posición del nodo en el mundo
-    public int gridX; // posición del nodo en el grid en x
-    public int gridY;  // posición del nodo en el grid en y
-
-    public int gCost;
-    public int hCost;
-    public Node parent;
-
-
-    public Node(bool _walkable, Vector3 _worldPos,int _gridX, int _gridY ){
-        walkable = _walkable;
-        worldPos = _worldPos;
-        gridX = _gridX;
-        gridY = _gridY;
-
-    }
-    
-    public int fCost
+    public class Node
     {
-        get
+        public CellInfo cell;
+        public int gCost;
+        public int hCost;
+        public Node parent;
+
+        public Node(CellInfo cell)
         {
-            return gCost+hCost;
+            this.cell = cell;
+            gCost = int.MaxValue;
+            hCost = 0;
+            parent = null;
         }
+
+        public int fCost => gCost + hCost;
     }
 }
+
